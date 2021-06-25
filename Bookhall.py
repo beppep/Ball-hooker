8cccf18f6d9b45aed7246fd1592c7e1902721feb
unknown <bror.beppe.persson@gmail.com>
2021-06-26 01:20:01 +0200

    awful graphics update

--- Bookhall.py
+++ Bookhall.py
@@ -12,32 +12,90 @@ time_step = 1.0/90
 resolution = (1200, 675)
 
 levels = [ #shape means radius btw
-[(1,-3), 1, [ #0
+[(2.7,-4), 0.6, [ #1
     [(8,-9), (8, 1)],
     [(8,0), (8, 1)],
     [(0,-4.5), (1, 4.5)],
     [(16,-4.5), (1, 4.5)],
-    [(3,-6), (2, 2)],
+    [(2.5,-6), (2, 2)],
     [(14.5,-4.5), (0.5, 3.5), ["win"]],
-    ],[
-    [(8,-5), [(-1,0),(1,-1),(1,1),(-1,1)]],
 ]],
-[(4.4,-4), 2, [ #1
+[(1,-3), 1, [ #2
     [(8,-9), (8, 1)],
     [(8,0), (8, 1)],
     [(0,-4.5), (1, 4.5)],
-    [(16,-4.5), (1, 4.5)],
-    [(3,-7), (3, 2)],
-    [(14,-7), (0.5, 0.5), ["win"]],
-    ],[
-    [(7,-6.5), [(-1,-3),(1,-3),(1,3),(-1, 3)]],
+    [(3,-6), (2, 2)],
+    [(15.5,-4.5), (0.5, 3.5), ["win"]],
+    [(9,-7), (1,1)],
 ]],
-[(3,-7.4), 2.2, [ #2: roof goal
+[(3,-7.4), 1.4, [ #3
     [(8,-9), (8, 1)],
     [(8,0), (8, 1)],
     [(0,-4.5), (1, 4.5)],
     [(16,-4.5), (1, 4.5)],
     [(8,-1.5), (7, 0.5), ["win"]],
+]],
+[(2,-7.4), 0.6, [ #4
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(11.5,-2), (3.5, 1), ["win"]],
+    [(7,-3), (1,2)],
+]],
+[(1,-3), 1, [ #5
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(3,-6), (2, 2)],
+    [(14.5,-2.5), (0.5, 1.5), ["win"]],
+    [(12,-6), (3, 2)],
+]],
+[(1,-3), 1, [ #6
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(3,-6), (2, 2)],
+    [(14.5,-2.5), (0.5, 1.5), ["win"]],
+    [(12,-6), (3, 2)],
+]],
+[(1,-6), 1, [ #7
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(2,-7.5), (2, 1)],
+    [(8,-6), (4, 3)],
+    [(13.5,-7.5), (1.5, 0.5), ["win"]],
+]],
+[(2,-7), 0.6, [ #8
+    [(8,-9), (8, 1.5)],
+    [(8,0), (8, 1.5)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(2,-2.5), (1, 1), ["win"]],
+    [(5,-5), (4,0.5)],
+    [(13,-6.5), (2,2)],
+]],
+[(2,-7), 1, [ #9: Low High
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(6,-3.5), (1, 3)],
+    [(10,-6.5), (1, 3)],
+    [(15,0), (1,9), ["win"]],
+]],
+[(2,-7), 1, [ #10: Low High
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(6,-3.5), (1, 3)],
+    [(10,-6.5), (1, 3)],
+    [(15,0), (1,9), ["win"]],
 ]],
 [(1,-3), 1, [ #3: lava floor
     [(8,-9), (8, 1)],
@@ -47,16 +105,6 @@ levels = [ #shape means radius btw
     [(2,-7), (2, 1)],
     [(8,-9), (4, 1), ["death"]],
     [(14,-7), (0.5, 0.5), ["win"]],
-]],
-[(1,-3), 1, [ #4: momentum jump
-    [(8,-9), (8, 1)],
-    [(4,0), (4, 1),], #left roof
-    [(12,0), (4, 1), ["nograb"]], #right roof
-    [(0,-4.5), (1, 4.5)],
-    [(16,-4.5), (1, 4.5), ["nograb"]],
-    [(2,-6), (2, 2)],
-    [(8,-2), (0.2, 2)],
-    [(14.5,-1.5), (0.5, 0.5), ["win"]],
 ]],
 [(2,-5), 1.4, [ #5: Slit
     [(8,-1.5), (0.5,2),["death"]],
@@ -68,21 +116,53 @@ levels = [ #shape means radius btw
     [(16,-4.5), (1, 4.5)],
     [(15,0), (1,9), ["win"]],
 ]],
-[(2,-7), 1, [ #6: Low High
-    [(8,-9), (8, 1)],
-    [(8,0), (8, 1)],
-    [(0,-4.5), (1, 4.5)],
-    [(16,-4.5), (1, 4.5)],
-    [(6,-3.5), (1, 3)],
-    [(10,-6.5), (1, 3)],
-    [(15,0), (1,9), ["win"]],
-]],
 [(2,-6), 1.6, [ #7: Lava Room
     [(8,-9), (8, 1),["death"]],
     [(8,0), (8, 1),["death"]],
     [(0,-4.5), (1, 4.5),["death"]],
     [(16,-4.5), (1, 4.5),["death"]],
     [(15,0), (1,9), ["win"]],
+]],
+[(4.4,-4), 2, [ #1
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(3,-7), (3, 2)],
+    [(14,-7), (0.5, 0.5), ["win"]],
+    ],[
+    [(7,-6.5), [(-1,-3),(1,-3),(1,3),(-1, 3)]],
+]],
+[(2,-6), 0, [ #10: stuff
+    [(8,-9), (8, 1)],
+    [(8,0), (8, 1)],
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5)],
+    [(3,-7), (2, 1)],
+    ],[
+    [(8,-1.2), [(-0.2,-0.2),(0.2,-0.2),(0.2,0.2),(-0.2,0.2)],"death"],
+    [(8.1,-1.7), [(-0.2,-0.2),(0.2,-0.2),(0,0.2)],"death"],
+    [(8,-2.2), [(-0.2,-0.2),(0.2,-0.2),(0.2,0.2),(-0.2,0.2)],"win"],
+    [(8.1,-2.6), [(-0.2,-0.2),(0.2,-0.2),(0.2,0.2),(-0.2,0.2)],"death"],
+    [(8.2,-3.1), [(-0.2,-0.2),(0.2,-0.2),(0.2,0.2),(-0.2,0.2)],"death"],
+]],
+[(1,-3), 1, [ #4: momentum jump
+    [(8,-9), (8, 1)],
+    [(4,0), (4, 1),], #left roof
+    [(12,0), (4, 1), ["nograb"]], #right roof
+    [(0,-4.5), (1, 4.5)],
+    [(16,-4.5), (1, 4.5), ["nograb"]],
+    [(2,-6), (2, 2)],
+    [(8,-2), (0.2, 2)],
+    [(14.5,-1.5), (0.5, 0.5), ["win"]],
+]],
+[(2,-3), 1, [ #9: Edge
+    [(1.5,-6.5), (0.5,1.5),["win"]],
+    [(8,-9), (8, 1),["death"]],
+    [(8,0), (8, 1),],
+    [(4,-4.5), (3, 0.5),["nograb"]],
+    [(0,-4.5), (1, 4.5),],
+    [(16,0), (1,9),],
 ]],
 [(1.5,-1.5), 2, [ #8: :)  x
     [(8,0), (8, 1),["nograb"]],
@@ -98,27 +178,6 @@ levels = [ #shape means radius btw
     [(3,-6), [(4,0.5),(5,1),(4,-2)],["nograb"],False,False],
     ],[
     [(3,-6), [(5,1),(4,-2),(4,1)]],
-]],
-[(2,-3), 1, [ #9: Edge
-    [(1.5,-6.5), (0.5,1.5),["win"]],
-    [(8,-9), (8, 1),["death"]],
-    [(8,0), (8, 1),],
-    [(4,-4.5), (3, 0.5),["nograb"]],
-    [(0,-4.5), (1, 4.5),],
-    [(16,0), (1,9),],
-]],
-[(2,-5), 0, [ #10: stuff
-    [(8,-9), (8, 1)],
-    [(8,0), (8, 1)],
-    [(0,-4.5), (1, 4.5)],
-    [(16,-4.5), (1, 4.5)],
-    [(3,-7), (2, 1)],
-    ],[
-    [(8,-2), [(-0.1,-0.1),(0.1,-0.1),(0.1,0.1),(-0.1,0.1)],"death"],
-    [(8,-2.2), [(-0.1,-0.1),(0.1,-0.1),(0.1,0.1),(-0.1,0.1)],"death"],
-    [(8,-2.4), [(-0.1,-0.1),(0.1,-0.1),(0.1,0.1),(-0.1,0.1)],"win"],
-    [(8,-2.6), [(-0.1,-0.1),(0.1,-0.1),(0.1,0.1),(-0.1,0.1)],"death"],
-    [(8,-2.8), [(-0.1,-0.1),(0.1,-0.1),(0.1,0.1),(-0.1,0.1)],"death"],
 ]],
 #[(2,-3), 1, [ #9: Edge
 #    [(8,0), (8, 1),],
@@ -136,11 +195,20 @@ levels = [ #shape means radius btw
 #     [(15,0), (1,9), ["win"]],
 # ]],
 ]
-pygame.init()
 
-def loadImage(name,r):
+pygame.init()
+clock = pygame.time.Clock()
+game_display = pygame.display.set_mode(resolution)#, pygame.FULLSCREEN)
+pygame.display.set_caption('Hall Booker!')
+
+
+def loadImage(name,r,r2=None, dontConvert=False):
     image = pygame.image.load("BookhallFiles/textures/"+name)
+    if r2==None:
+    	r2=r
     image = pygame.transform.scale(image, (int(r*2*ppm), int(r*2*ppm)))
+    if not dontConvert:
+        image=image.convert() #reduces lag
     return image
 
 def rot_center(image, angle):
@@ -166,6 +234,7 @@ class Game():
         if(num>len(levels)-1):
             print(time.time() - time_start)
         self.currentLevel = Level(*levels[num])
+        self.currentLevel.loadImages(num//lpr)
 
     def exitLevel(self):
         game.currentLevel.killPlayer()
@@ -193,6 +262,8 @@ class Game():
     def draw(self):
         if self.currentLevel:
             self.currentLevel.draw()
+        else:
+            game_display.fill((100,200,200))
 
 class Block():
 
@@ -209,10 +280,11 @@ class Block():
         else:
             self.body = world.CreateStaticBody(position=position,shapes=self.bodyShape,userData=self)
 
-    def draw(self):
+    def draw(self, surf):
         vertices = []
         for i in range(len(self.shape)):
             vertices.append((self.body.GetWorldPoint(self.shape[i])[0]*ppm, -self.body.GetWorldPoint(self.shape[i])[1]*ppm))
+        """
         color=[50,50,100]
         if("win" in self.blockType):
             color[1]+=150
@@ -222,6 +294,8 @@ class Block():
             color[1]-=50
             color[2]-=50
         pygame.draw.polygon(game_display, color, vertices)
+        """
+        pygame.draw.polygon(surf, (255,255,255), vertices)
 #ögon utstickna av de andra omagiska penguins. exiled 500 år sedan. 
 class Level():
 
@@ -257,14 +331,47 @@ class Level():
     def update(self):
         self.player.update()
 
+    def loadImages(self, zone):
+        self.backgroundImage = loadImage("background"+str(zone)+".png",8,4.5)
+        self.groundImage = loadImage("ground"+str(zone)+".png",8,4.5)
+        self.winImage = loadImage("win"+str(zone)+".png",8,4.5)
+        if zone>=1:
+            self.lavaImage = loadImage("lava"+str(zone)+".png",8,4.5)
+        else:
+            self.lavaImage = None
+
     def draw(self):
-        for block in self.blocks+self.dynamicBlocks:
-            block.draw()
+        game_display.blit(self.backgroundImage, (0,0))
+
+        for paintjob in [(self.winImage, lambda x:"win" in x), (self.lavaImage, lambda x:"death" in x), (self.groundImage, lambda x:not "win" in x and not "death" in x)]:
+            groundImage = paintjob[0]
+            groundSurf = pygame.Surface(resolution, pygame.SRCALPHA, 32).convert_alpha()
+            groundSurf.fill((255,255,255,0))
+            skip=1
+            for block in self.blocks+self.dynamicBlocks:
+                if paintjob[1](block.blockType):
+                    block.draw(groundSurf)
+                    skip=0
+            if skip:
+                continue
+            badgraphics = 1 #try this to not lag
+            if badgraphics:
+                groundSurf = pygame.transform.scale(groundSurf, (160, 90)) #
+                groundImage = pygame.transform.scale(groundImage, (160, 90)) #
+            
+            groundSurf.blit(groundImage,(0,0),None,pygame.BLEND_RGBA_MULT) # blitting large convert_alpha images is laggy
+
+            if badgraphics:
+                groundSurf = pygame.transform.scale(groundSurf, resolution) #
+
+            game_display.blit(groundSurf, (0,0))
+
+        
         self.player.draw()
 
 class Player():
 
-    sprite = loadImage("Book.png", 0.5)
+    sprite = loadImage("Book.png", 0.5, dontConvert=True)
 
     def __init__(self, spawnpoint, spawnrotation=math.pi/4):
         self.body = world.CreateDynamicBody(fixtures=Box2D.b2FixtureDef(shape=Box2D.b2CircleShape(radius=0.5),density=1.0, friction=0.1, restitution=0.2),bullet=True,position=spawnpoint, userData=self)
@@ -297,39 +404,37 @@ class Player():
     def update(self):
         pressed = pygame.key.get_pressed()
         """
-        if pressed[pygame.K_d]:
+        if pressed[pygame.K_RIGHT]:
             self.body.ApplyTorque(-1, wake=True)
-        if pressed[pygame.K_a]:
+        if pressed[pygame.K_LEFT]:
             self.body.ApplyTorque(1, wake=True)
         """
         if pressed[pygame.K_SPACE]:
             if self.rope:
-                actualLength = math.sqrt((self.rope.anchorA[0]-self.rope.anchorB[0])**2 + (self.rope.anchorA[1]-self.rope.anchorB[1])**2)
-                if actualLength > 0.1:
-                    k=abs(self.rope.maxLength-actualLength)
-                    self.rope.SetMaxLength(actualLength)
-                    #print(k)
-                    #self.rope.length=actualLength-0.08
-                    if((k)<0.04):
-                        vect = ((self.rope.anchorB[0] - self.rope.anchorA[0])*12, (self.rope.anchorB[1] - self.rope.anchorA[1])*12)
-                        self.body.ApplyForce(force=vect, point=self.rope.anchorA, wake=True)
-                        self.rope.bodyB.ApplyForce(force=(-vect[0],-vect[1]), point=self.rope.anchorB, wake=True)
-                    #print(self.rope.length)
+                if pressed[pygame.K_UP]:
+                	actualLength = math.sqrt((self.rope.anchorA[0]-self.rope.anchorB[0])**2 + (self.rope.anchorA[1]-self.rope.anchorB[1])**2)
+	                if actualLength > 0.1:
+	                    k=abs(self.rope.maxLength-actualLength)
+	                    self.rope.SetMaxLength(actualLength)
+	                    #print(k)
+	                    if((k)<0.04):
+	                        vect = ((self.rope.anchorB[0] - self.rope.anchorA[0])*12, (self.rope.anchorB[1] - self.rope.anchorA[1])*12)
+	                        self.body.ApplyForce(force=vect, point=self.rope.anchorA, wake=True)
+	                        self.rope.bodyB.ApplyForce(force=(-vect[0],-vect[1]), point=self.rope.anchorB, wake=True)
+	                    #print(self.rope.length)
             else:
                 self.hook()
-                #print("hooked")
-        if pressed[pygame.K_e]:
+        else:
             if self.rope:
                 world.DestroyJoint(self.rope)
                 self.rope = None
         if pressed[pygame.K_r]:
             game.imminentDeath=True
-
         if self.rope:
             vect = (self.rope.anchorB[0] - self.rope.anchorA[0], self.rope.anchorB[1] - self.rope.anchorA[1])
             a = self.body.angle
             ourPoint=self.body.position+(math.cos(a)/2,math.sin(a)/2)
-            self.body.ApplyForce(force=vect, point=ourPoint, wake=True)
+            self.body.ApplyForce(force=vect, point=ourPoint, wake=True) #?
 
     def draw(self):
         image = rot_center(self.sprite, self.body.angle)
@@ -362,10 +467,6 @@ class myContactListener(Box2D.b2ContactListener):
     def PostSolve(self, contact, impulse):
         pass
 
-clock = pygame.time.Clock()
-game_display = pygame.display.set_mode(resolution)#, pygame.FULLSCREEN)
-pygame.display.set_caption('Hall Booker!')
-
 managers={
     "":pygame_gui.UIManager(resolution), #
     "l":pygame_gui.UIManager(resolution), #Level select
@@ -377,7 +478,7 @@ level_buttons = []
 lpr = 10 #levels per row
 for i in range(len(levels)//lpr+1):
     for j in range(min(lpr,len(levels)-lpr*i)):
-        button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100+j*100, 100+i*100), (100, 100)),text="Level "+str(i*lpr+j),manager=managers["l"])
+        button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100+j*100, 100+i*100), (100, 100)),text="Level "+str(i*lpr+j+1),manager=managers["l"])
         level_buttons.append(button)
 speedrun_textbox = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((20, 20), (200, 64)),html_text="Speeds: <br> 0:00",manager=managers["p"])
 
@@ -413,7 +514,7 @@ while jump_out == False:
     world.Step(time_step, 20,10)
     world.ClearForces()
 
-    game_display.fill((200,200,250))
+
     game.draw()
     manager.draw_ui(game_display)
 
